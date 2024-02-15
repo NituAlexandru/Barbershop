3b09e59b7a958ce672f8f34b0a6328a23f748cf5
Nitu Alexandru <nitzu_alexandru@yahoo.com>
2024-02-15 22:30:05 +0200

    Created mobile backdrop & nav
    -added html structure
    -created JS logic
    -added CSS dislpay

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
-document.addEventListener('DOMContentLoaded', function () {
-    let currentSlide = 0;
-    const slides = document.querySelectorAll('.slide');
+document.addEventListener("DOMContentLoaded", function () {
+  let currentSlide = 0;
+  const slides = document.querySelectorAll(".slide");
 
-    function showSlide(index) {
-        slides.forEach((slide) => {
-            slide.style.opacity = '0';
-        });
-        slides[index].style.opacity = '1';
+  function showSlide(index) {
+    slides.forEach((slide) => {
+      slide.style.opacity = "0";
+    });
+    slides[index].style.opacity = "1";
+  }
+
+  setInterval(() => {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+  }, 4000);
+
+  function openNav() {
+    const menu = document.getElementById("mobile-backdrop");
+    if (menu) menu.classList.remove("hidden");
+  }
+
+  function closeNav() {
+    const menu = document.getElementById("mobile-backdrop");
+    if (menu) menu.classList.add("hidden");
+  }
+
+  const burgerMenuButton = document.querySelector(".burger-menu");
+  if (burgerMenuButton) burgerMenuButton.addEventListener("click", openNav);
+
+  const closeButton = document.querySelector(".backdrop-close-btn");
+  if (closeButton) closeButton.addEventListener("click", closeNav);
+
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      closeNav();
     }
+  });
 
-    setInterval(() => {
-        currentSlide = (currentSlide + 1) % slides.length;
-        showSlide(currentSlide);
-    }, 4000);
+  const navLinks = document.querySelectorAll(
+    ".mobile-nav-link, .global-aside-links .link"
+  );
+  navLinks.forEach(function (link) {
+    link.addEventListener("click", function () {
+      closeNav();
+    });
+  });
 });
